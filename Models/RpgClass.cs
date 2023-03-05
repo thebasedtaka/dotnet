c835051d140ba6985544699c8ceebaf69412beee
thebasedtaka <darktaka9998@gmail.com>
2023-03-05 12:38:39 -0500

    CRUD COMPLETE

---
+++ Models/RpgClass.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+
+        Mage = 2,
+
+        Cleric = 3,
+    }
+}
